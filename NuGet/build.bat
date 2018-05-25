1fc27bd8f060c37cb0ea561aef7b608af353cead
moberacker <markus.oberacker@ptv.de>
2018-05-25 16:17:34 +0200

    change targets

--- NuGet/build.bat
+++ NuGet/build.bat
@@ -1,8 +1,9 @@
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 
-%MSBUILD% build.proj /target:NuGetRestore
-%MSBUILD% /p:Configuration=Release ..\Log4ALA.sln
-%MSBUILD% build.proj /target:NuGetPack /property:Configuration=Release;RELEASE=true
+REM %MSBUILD% build.proj /target:NuGetRestore
+REM %MSBUILD% /p:Configuration=Release ..\Log4ALA.sln
+REM %MSBUILD% build.proj /target:NuGetPack /property:Configuration=Release;RELEASE=true
 REM PackageVersion=4.5.0;PatchVersion=0;PatchCoreVersion=0
-
+del *.nupkg
+%MSBUILD% build.proj /target:BuildAll /property:Configuration=Release;RELEASE=true;MajorVersion=2;MinorVersion=0;PatchVersion=8
 pause
