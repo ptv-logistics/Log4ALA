672728a00dd3d15f02b063ac90cb7b5890c9b9ec
moberacker <markus.oberacker@ptv.de>
2018-05-25 20:00:33 +0200

    changed

--- Log4ALATest.Core/LoggerTests.cs
+++ Log4ALATest.Core/LoggerTests.cs
@@ -31,14 +31,6 @@ namespace Log4ALATest.Core
             var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
             XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
 
-            //Log message as anonymous type... the properties will then be mapped to Azure Log Analytic properties/columns.
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    alaLogger1.Info(new { id = $"log-{i}", message = $"test-{i}" });
-            //}
-
-            //System.Console.WriteLine("done1");
-            //LogManager.Shutdown();
 
             ////Log messages with semicolon separated key=value strings...the keys will then be mapped to Azure Log Analytic properties/columns.
             for (int i = 0; i < 3; i++)
@@ -49,74 +41,12 @@ namespace Log4ALATest.Core
 
             System.Console.WriteLine("done2");
 
-            //Log messages with semicolon separated key=value strings and duplicate key detection... the duplicate keys in the following example 
-            //will be mapped to Azur Log Analytic properties/columns message_Duplicate0 and message_Duplicate1.
-
-            //int iii = 0;
-            //int i = 0;
-
-            //for (int ii = 0; ii < 1000; ii++)
-            //{
-            //    Task.Run(async () =>
-            //    {
-            //        System.Console.WriteLine($"sleep{++iii}");
-            //        string[] lines = File.ReadAllLines("c:\\users\\mob\\downloads\\EMCore_2017-07-19_001311_2017-07-19_061311.log");
-            //        System.Console.WriteLine($"lines {lines.Length}");
-            //        //System.Threading.Thread.Sleep(new TimeSpan(0, 0, 2));
-            //        System.Console.WriteLine($"run{iii}");
-
-            //    //foreach (var line in lines)
-            //    //{
-
-
-            //        Parallel.ForEach(lines, (line) =>
-            //        {
-            //        string test = line;//.Replace(" - ", "|").Split("|".ToCharArray())[1];
-            //                           //System.Console.WriteLine(test);
-            //        alaLogger2.Info($"{test}");
-            //        System.Console.WriteLine($"step{i}");
-            //        //alaLogger2.Debug($"id=log-{i}; message=test-{i}; message=test-{i}; message=test-{i}");
-
-            //        });
-            //    //}
-            //        System.Console.WriteLine($"end {iii}");
-
-
-
-            //    });
-
-
-
-
-            //}
-
-            //System.Console.WriteLine("done3");
-
-            ////Log message as json string ...the json properties will then be mapped to Azure Log Analytic properties/columns.
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    alaLogger3.Info($"{{\"id\":\"log-{i}\", \"message\":\"test-{i}\"}}");
-            //}
 
 
 
             System.Console.WriteLine("done4");
 
-            //System.Threading.Thread.Sleep(new TimeSpan(0, 0, 10));
-
-
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    alaLogger2.Info($"id=log-{i}; message=test-{i}; percent={i}.{i}; count={i}; testdate={DateTime.Now.ToString()};testdate2={DateTime.Now.ToString("o")};testdate4=Monday, July 10, 2017 8:47 AM;testbool={Boolean.TrueString};testbool2=True");
-            //}
-
-
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    System.Threading.Thread.Sleep(new TimeSpan(0, 0, 4));
-            //    System.Console.WriteLine($"{i}...");
-            //}
-
+  
 
             System.Threading.Thread.Sleep(new TimeSpan(1, 5, 0));
 
