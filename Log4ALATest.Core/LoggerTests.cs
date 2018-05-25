788f451259ae83ea131900b83653d75b01194335
moberacker <markus.oberacker@ptv.de>
2018-05-25 20:03:14 +0200

    remove compiler flags

--- Log4ALATest.Core/LoggerTests.cs
+++ Log4ALATest.Core/LoggerTests.cs
@@ -2,26 +2,17 @@
 using System;
 using log4net.Config;
 using System.IO;
-#if NETCOREAPP2_0
 using log4net.Repository;
 using System.Reflection;
-#endif
 
 namespace Log4ALATest.Core
 {
 
     class LoggerTests
     {
-#if NETCOREAPP2_0
         private static ILoggerRepository REPOSITORY = log4net.LogManager.CreateRepository(Assembly.GetEntryAssembly(), typeof(log4net.Repository.Hierarchy.Hierarchy));
-#endif
 
-
-#if NETCOREAPP2_0
         private static ILog alaLogger2 = LogManager.GetLogger(REPOSITORY.Name, "Log4ALALogger_2");
-#else
-        private static ILog alaLogger2 = LogManager.GetLogger("Log4ALALogger_2");
-#endif
 
         static void Main(string[] args)
         {
