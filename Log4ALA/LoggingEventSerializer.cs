b4cc93ba3d14a60b490df4754f457bd64606af10
moberacker <markus.oberacker@ptv.de>
2018-11-26 15:42:58 +0100

    changed SerializeLoggingEvent

--- Log4ALA/LoggingEventSerializer.cs
+++ Log4ALA/LoggingEventSerializer.cs
@@ -70,7 +70,14 @@ namespace Log4ALA
                 }
                 else
                 {
-                    payload.Add(appender.coreFields.MiscMessageFieldName, loggingEvent.MessageObject);
+                    if (loggingEvent.MessageObject != null && loggingEvent.MessageObject is log4net.Util.SystemStringFormat)
+                    {
+                        payload.Add(appender.coreFields.MiscMessageFieldName, loggingEvent.RenderedMessage);
+                    }
+                    else
+                    {
+                        payload.Add(appender.coreFields.MiscMessageFieldName, loggingEvent.MessageObject);
+                    }
                 }
             }
 
