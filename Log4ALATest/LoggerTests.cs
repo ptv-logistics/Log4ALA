963ab40eb40f1c97aec1cc48c300b14a2c5a40b3
Markus Oberacker <markus.oberacker@ptv.de>
2019-04-09 10:02:53 +0200

    simplify log4net initialization

--- Log4ALATest/LoggerTests.cs
+++ Log4ALATest/LoggerTests.cs
@@ -1,32 +1,21 @@
-﻿using log4net;
+using log4net;
 using System;
-using log4net.Config;
-using System.IO;
-using log4net.Repository;
-using System.Reflection;
 
 namespace Log4ALATest.Core
 {
 
     class LoggerTests
     {
-        private static ILoggerRepository REPOSITORY = log4net.LogManager.CreateRepository(Assembly.GetEntryAssembly(), typeof(log4net.Repository.Hierarchy.Hierarchy));
 
-        private static ILog alaLogger2 = LogManager.GetLogger(REPOSITORY.Name, "Log4ALALogger_2");
+        private static ILog alaLogger2 = LogManager.GetLogger("Log4ALALogger_2");
 
         static void Main(string[] args)
         {
-
-            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
-            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
-
-
             ////Log messages with semicolon separated key=value strings...the keys will then be mapped to Azure Log Analytic properties/columns.
             for (int i = 0; i < 100; i++)
             {
                 alaLogger2.Info($"id=log-{i}; message=netstandard2-test-{i}; intTest={i}; doubleTest={i}.{5}");
             }
-
 
             System.Console.WriteLine("done");
 
