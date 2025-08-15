3d04fbe9de935c36acfe0fd7c178b1f528944466
Markus OBERACKER (PTV Logistics) <Markus.Oberacker@ptvlogistics.com>
2025-08-15 07:41:49 +0200

    changed version

--- NuGet/build.bat
+++ NuGet/build.bat
@@ -1,4 +1,4 @@
-SET MSBUILDPATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
+SET MSBUILDPATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin"
 SET MSBUILD=%MSBUILDPATH%\MSBuild.exe
 
 
@@ -6,5 +6,5 @@ REM %MSBUILD% build.proj /target:NuGetRestore
 REM %MSBUILD% /p:Configuration=Release ..\Log4ALA.sln
 REM %MSBUILD% build.proj /target:NuGetPack /property:Configuration=Release;RELEASE=true
 REM PackageVersion=4.5.0;PatchVersion=0;PatchCoreVersion=0
-%MSBUILD% build.proj /target:BuildAll /property:Configuration=Release;RELEASE=true;MajorVersion=2;MinorVersion=9;PatchVersion=1;MSBuildPath=%MSBUILDPATH%
+%MSBUILD% build.proj /target:BuildAll /property:Configuration=Release;RELEASE=true;MajorVersion=2;MinorVersion=10;PatchVersion=0;MSBuildPath=%MSBUILDPATH%
 pause
